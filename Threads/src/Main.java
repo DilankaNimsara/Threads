c5494465a44a921471b7c8acacf2e8aea02bc0ec
Dilanka Amarasena <dilanka_a@epic.lk>
2021-01-12 14:58:33 +0530

    task threds done

--- Threads/src/Main.java
+++ Threads/src/Main.java
@@ -10,7 +10,7 @@ public class Main {
         Thread paperRefill = new PaperRefill();
         paperRefill.start();
 
-        for (int i = 1; i <= 5; j++) {
+        for (int i = 1; i <= 5; i++) {
             Thread student = new Student((Printer) printer);
             student.setName("Student " + i);
             student.start();
